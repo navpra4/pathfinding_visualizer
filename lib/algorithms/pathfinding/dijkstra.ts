25702e4d159b1d075eefcd0e17031c85996d9935
navpra4 <naveenpra4@gmail.com>
2024-08-03 15:07:44 +0530

    added dijkstra and a_star

---
+++ lib/algorithms/pathfinding/dijkstra.ts
@@ -0,0 +1,46 @@
+import { getUntraversedNeighbours } from "@/lib/getUntraversedNeighbours";
+import { dropFromQueue, isEqual } from "@/lib/helpers";
+import { GridType, TileType } from "@/lib/types";
+
+
+export const dijkstra = (
+  grid: GridType,
+  startTile: TileType,
+  endTile: TileType
+) => {
+  const traversedTiles = []; 
+  const base = grid[startTile.row][startTile.col];
+  base.distance = 0;
+  base.isTraversed = true; 
+  const untraversedTiles = [base]; 
+
+  while (untraversedTiles.length > 0) {
+    untraversedTiles.sort((a, b) => a.distance - b.distance); 
+    const currentTile = untraversedTiles.shift(); 
+    if (currentTile) {
+      if (currentTile.isWall) continue; 
+      if (currentTile.distance === Infinity) break; 
+      currentTile.isTraversed = true; 
+      traversedTiles.push(currentTile); 
+      if (isEqual(currentTile, endTile)) break; 
+      const neighbors = getUntraversedNeighbours(grid, currentTile); 
+      for (let i = 0; i < neighbors.length; i += 1) {
+        if (currentTile.distance + 1 < neighbors[i].distance) {
+          dropFromQueue(neighbors[i], untraversedTiles); 
+          neighbors[i].distance = currentTile.distance + 1; 
+          neighbors[i].parent = currentTile;
+          untraversedTiles.push(neighbors[i]); 
+        }
+      }
+    }
+  }
+
+  const path = []; 
+  let current = grid[endTile.row][endTile.col];
+  while (current !== null) {
+    current.isPath = true; 
+    path.unshift(current); 
+    current = current.parent!; 
+  }
+  return { traversedTiles, path }; 
+};
--- lib/runPathFindingAlgo.ts
+++ lib/runPathFindingAlgo.ts
@@ -1,5 +1,7 @@
+import { aStar } from "./algorithms/pathfinding/aStar";
 import { bfs } from "./algorithms/pathfinding/bfs";
 import { dfs } from "./algorithms/pathfinding/dfs";
+import { dijkstra } from "./algorithms/pathfinding/dijkstra";
 import { AlgoirthmType, GridType, TileType } from "./types"
 
 export const runPathFindingAlgo = ({
@@ -17,7 +19,11 @@ export const runPathFindingAlgo = ({
         case 'BFS':
             return bfs(grid, startTile, endTile);
         case 'DFS':
-            return dfs(grid, startTile, endTile);   
+            return dfs(grid, startTile, endTile); 
+        case 'DIJKSTRA':
+            return dijkstra(grid, startTile, endTile);
+        case 'A_STAR':
+            return aStar(grid, startTile, endTile);  
         default:
             return bfs(grid, startTile, endTile);
     }
